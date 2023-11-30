99f3753b7258973a7b9cbcc8a751622d7f5994d0
Subhankar Dey <subhankardey1122@gmail.com>
2023-11-30 23:11:23 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,37 @@
 import React from "react";
 import { useUserContext } from "./UserContext";
 import { Container, Typography } from "@mui/material";
+import "../styles/customStyles.css"; // Make sure to import the CSS
 
 const Home = () => {
   const { user } = useUserContext();
 
   return (
-    <Container maxWidth="sm" sx={{ mt: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        Home Page
-      </Typography>
-      {user && (
-        <Typography variant="body1">Welcome, {user.username}</Typography>
-      )}
-    </Container>
+    <div className="register-background homepage-background">
+      {" "}
+      {/* Use same background class */}
+      <Container
+        maxWidth="sm"
+        sx={{
+          mt: 4,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div className="homepage-text">
+          {" "}
+          {/* Apply the new animation class */}
+          <Typography variant="h4" gutterBottom>
+            Home Page
+          </Typography>
+          {user && (
+            <Typography variant="body1">Welcome, {user.username}</Typography>
+          )}
+        </div>
+      </Container>
+    </div>
   );
 };
 
